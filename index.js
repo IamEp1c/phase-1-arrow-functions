dcd0620bbd518062ee34ef859e2da9d7c6e67297
Waqas Anjum <Qanjum96@gmail.com>
2022-04-05 20:05:37 -0400

    commit tree 94f15b7ce676409f36f1a9742084ec88e60a2f2d

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+    return 2000 / 100
+}
+// divide();
+
+let square = (parameter) => {
+    return parameter * parameter
+}
+
+let add = (num1, num2) => {
+    return num1 + num2
+}
